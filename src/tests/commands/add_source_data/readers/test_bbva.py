e72443f7a9f82dda1242a583ca6b700f150efec0
alguerre <alguerre@outlook.com>
2023-08-13 16:25:28 +0200

    feat: New bbva reader

---
+++ src/tests/commands/add_source_data/readers/test_bbva.py
@@ -0,0 +1,8 @@
+from src.commands.add_source_data.readers import Bbva
+
+
+def test_bbva(sample_bank_history, sample_files):
+    assert (
+        Bbva().read(sample_files["bbva"]).drop(["origin"], axis=1).to_dict()
+        == sample_bank_history.to_dict()
+    )
